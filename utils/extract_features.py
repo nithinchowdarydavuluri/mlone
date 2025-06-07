678df52c1e7b949d40f11a93c44d3c5d10da9c40
Davuluri Nithin Chowdary <154059792+nithinchowdarydavuluri@users.noreply.github.com>
2025-06-07 16:03:07 +0530

    Update extract_features.py

--- utils/extract_features.py
+++ utils/extract_features.py
@@ -1,1 +1,11 @@
+# utils/extract_features.py
+import librosa
+import numpy as np
+
+def extract_mel_spectrogram(file_path):
+    y, sr = librosa.load(file_path, duration=30)
+    mel = librosa.feature.melspectrogram(y=y, sr=sr)
+    mel_db = librosa.power_to_db(mel, ref=np.max)
+    mel_db = mel_db[:128, :128]
+    return mel_db if mel_db.shape == (128, 128) else None
 
